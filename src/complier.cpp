f63fee3f889b8c3fad99d7fb86fb4b2178281181
joyran <2574202043@qq.com>
2024-06-09 23:25:37 +0800

    修改未初始化grammar的错误

--- src/complier.cpp
+++ src/complier.cpp
@@ -89,25 +89,68 @@ void lexer_test() {
 }
 
 void parser_test() {
-    std::ifstream infile("/home/joyran/Course-Design-of-Compiler-Principles/in_out_txt/parser_in.txt");
-    std::ofstream outfile("/home/joyran/Course-Design-of-Compiler-Principles/in_out_txt/parser_out.txt");
+    std::ifstream infile_parser("/home/joyran/Course-Design-of-Compiler-Principles/in_out_txt/parser_in.txt");
+    std::ofstream outfile_parser("/home/joyran/Course-Design-of-Compiler-Principles/in_out_txt/parser_out.txt");
 
-    if (!infile || !outfile) {
+    if (!infile_parser || !outfile_parser) {
         std::cerr << "无法打开输入或输出文件" << std::endl;
         return;
     }
 
-    std::string input((std::istreambuf_iterator<char>(infile)), std::istreambuf_iterator<char>());
+    std::string input((std::istreambuf_iterator<char>(infile_parser)), std::istreambuf_iterator<char>());
     Lexer lexer(input);
     lexer.getTokens();
 
-    Grammar grammar;  // 初始化Grammar对象
+
+
+
+    // 初始化Grammar对象
+    std::ifstream infile("/home/joyran/Course-Design-of-Compiler-Principles/in_out_txt/grammar_in.txt");  // 输入文件
+    std::ofstream outfile("/home/joyran/Course-Design-of-Compiler-Principles/in_out_txt/grammar_out.txt");  // 输出文件
+
+    // 检查文件是否成功打开
+    if (!infile.is_open() || !outfile.is_open()) {
+        std::cerr << "Error opening files!" << std::endl;
+    }
+
+    // 重定向标准输入和输出到文件
+    std::streambuf* cinbuf = std::cin.rdbuf();
+    std::cin.rdbuf(infile.rdbuf());
+
+    std::streambuf* coutbuf = std::cout.rdbuf();
+    std::cout.rdbuf(outfile.rdbuf());
+
+    Grammar grammar;
+
+    // 读取并处理数据
+    grammar.Get_Vn();
+    grammar.Get_Vt();
+    grammar.Get_Gram();
+    grammar.Get_Vnull();
+    grammar.Get_First();
+    grammar.Get_Follow();
+    grammar.Get_Select();
+
+    // 恢复标准输入和输出
+    std::cin.rdbuf(cinbuf);
+    std::cout.rdbuf(coutbuf);
+
+    std::cout<<"grammmar test Success!!!"<<endl;
+
+    // 关闭文件
+    infile.close();
+    outfile.close();
+
+
+   
     SymTable symtbl(lexer);  // 初始化SymTable对象
     Parser parser(lexer.tokens, grammar, lexer, symtbl);  // 使用带参数的构造函数
+    std::cout<<"LL(1) start"<<"\n";
     parser.LL1();  // 执行LL1解析
-
+    
+    std::cout<<"LL(1) completed"<<"\n";
     for (const auto& quat : parser.quats) {
-        outfile << "(" << lexer.symbolNames[quat.op] << ", "
+        outfile_parser << "(" << lexer.symbolNames[quat.op] << ", "
                 << (quat.a == -1 ? "_" : lexer.symbolNames[quat.a]) << ", "
                 << (quat.b == -1 ? "_" : lexer.symbolNames[quat.b]) << ", "
                 << (quat.res == -1 ? "_" : lexer.symbolNames[quat.res]) << ")\n";
