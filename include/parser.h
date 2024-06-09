6e0944c2a74b83b3df028ec57080a9c7164f914f
joyran <2574202043@qq.com>
2024-06-09 23:01:36 +0800

    引用问题

--- include/parser.h
+++ include/parser.h
@@ -7,55 +7,64 @@
 #include "symbolTable.h"
 #include "optimizequa.h"
 
+// Action 结构体，用于存储操作信息
 struct Action {
-    int pos;
-    int type = 0;
-    int object;
-    int tokenid;
+    int pos;        // 操作位置
+    int type = 0;   // 操作类型，默认为 0
+    int object;     // 操作对象
+    int tokenid;    // 对应的标记 ID
 };
 
+// Quat 结构体，用于表示四元式
 struct Quat {
-    int op, a, b, res;
-    Quat(int op = 0, int a = 0, int b = 0, int res = 0) : op(op), a(a), b(b), res(res) {}
+    int op, a, b, res;  // 操作符，操作数1，操作数2，结果
+    Quat(int op = 0, int a = 0, int b = 0, int res = 0) 
+        : op(op), a(a), b(b), res(res) {}
 };
 
+// 操作类型的枚举
 enum { GEQ = 1, PUSH, ASSI, IF, ELSE, ENDIF, LAB, GOTO, WE, WH, DO };
 
+// 解析器类
 class Parser {
 public:
-    void Get_Action();
-    void Debug_LL1(std::stack<int>, int, int);
-    void Get_LL1Table();
-    void LL1();
-    void GEQ(int, int);
-    void PUSH(int, int);
-    void ASSI();
-    void IF();
-    void ELSE();
-    void ENDIF();
-    void LAB();
-    void GOTO();
-    void WE();
-    void WH();
-    void DO();
-    void Get_Quats();
-    void Print_Quat(Quat);
-    void Print_Act(Action);
-    void Debug_Quats(std::stack<int>, std::stack<Action>, int, int);
-    int Get_Type(int, int);
+    Parser(const std::vector<Token>& tokens, const Grammar& grammar, const Lexer& lexer, const SymTable& symtbl)
+        : tokens(tokens), gram(grammar), lex(lexer), symtbl(symtbl) {
+        Get_LL1Table();
+    }
 
-private:
-    std::map<std::pair<int, int>, int> LL1Table;
-    Grammar gram;
-    Lexer lex;
-    std::vector<Token> tokens;
-    std::stack<int> SEM;
-    std::stack<int> SToken;
-    int cnt_t = 0; // for encode t1..t2
-    std::vector<Quat> quats;
-    Action acts[maxn];
-    std::vector<std::string> acttype{"GEQ", "PUSH", "ASSI", "IF", "ELSE", "ENDIF", "LAB", "GOTO", "WE", "WH", "DO"};
-    SymTable symtbl;
+    void Get_Action();                       // 获取操作
+    void Debug_LL1(std::stack<int>, int, int); // 调试 LL1 解析
+    void Get_LL1Table();                     // 获取 LL1 分析表
+    void LL1();                              // LL1 解析
+    void GEQ(int, int);                      // GEQ 操作
+    void PUSH(int, int);                     // PUSH 操作
+    void ASSI();                             // ASSI 操作
+    void IF();                               // IF 操作
+    void ELSE();                             // ELSE 操作
+    void ENDIF();                            // ENDIF 操作
+    void LAB();                              // LAB 操作
+    void GOTO();                             // GOTO 操作
+    void WE();                               // WHILE 结束操作
+    void WH();                               // WHILE 开始操作
+    void DO();                               // DO 操作
+    void Get_Quats();                        // 获取四元式
+    void Print_Quat(Quat);                   // 打印四元式
+    void Print_Act(Action);                  // 打印操作
+    void Debug_Quats(std::stack<int>, std::stack<Action>, int, int); // 调试四元式
+    int Get_Type(int, int);                  // 获取类型
+
+    std::map<std::pair<int, int>, int> LL1Table; // LL1 分析表
+    Grammar gram;                            // 文法对象
+    Lexer lex;                               // 词法分析器对象
+    std::vector<Token> tokens;               // 标记向量
+    std::stack<int> SEM;                     // 语义栈
+    std::stack<int> SToken;                  // 标记栈
+    int cnt_t = 0;                           // 临时变量计数器
+    std::vector<Quat> quats;                 // 四元式向量
+    Action acts[maxn];                       // 操作数组
+    std::vector<std::string> acttype{"GEQ", "PUSH", "ASSI", "IF", "ELSE", "ENDIF", "LAB", "GOTO", "WE", "WH", "DO"}; // 操作类型字符串
+    SymTable symtbl;                         // 符号表
 };
 
 #endif // _PARSER_H
--- include/symbolTable.h
+++ include/symbolTable.h
@@ -6,21 +6,24 @@
 
 using namespace std;
 
+// 枚举类型，用于表示不同的数据类型
 enum { INT, DOUBLE, CHAR, BOOL, ARRAY, FUNCTION, STRUCT };
 
+// 记录类，表示符号表中的一条记录
 class Record {
 public:
-    int name;
-    int type;
-    int range;
-    int line = 0;
-    int ival;
-    double dval;
-    char cval;
-    bool bval;
-    int low, up, ctp, clen;
-    int level = 0, foff, fn = 0, param, entry;
+    int name;   // 变量名的ID
+    int type;   // 变量的类型（INT、DOUBLE、CHAR、BOOL、ARRAY、FUNCTION、STRUCT）
+    int range;  // 变量的作用域范围
+    int line = 0; // 变量所在行
+    int ival;   // 整数类型的值
+    double dval; // 双精度类型的值
+    char cval;  // 字符类型的值
+    bool bval;  // 布尔类型的值
+    int low, up, ctp, clen; // 数组相关的下界、上界、类型和长度
+    int level = 0, foff, fn = 0, param, entry; // 函数相关的级别、偏移量、函数编号、参数和入口
 
+    // 初始化变量的值
     void Init_Var(double v) {
         switch (type) {
             case INT: ival = v; break;
@@ -30,6 +33,7 @@ public:
         }
     }
 
+    // 初始化数组的属性
     void Init_Arr(int a, int b, int c, int d) {
         type = ARRAY;
         low = a;
@@ -38,6 +42,7 @@ public:
         clen = d;
     }
 
+    // 初始化函数的属性
     void Init_Fun(int a, int b, int c, int d) {
         type = FUNCTION;
         foff = a;
@@ -47,19 +52,29 @@ public:
     }
 };
 
+// 符号表类，管理所有的符号表记录
 class SymTable {
 public:
-    Lexer lex;
-    vector<map<int, vector<Record>>> Infos;
-    vector<string> lines;
-    set<string> Def{"int", "double", "char", "bool"};
-    vector<Record> synbl;
-    vector<Record> arr;
+    Lexer lex; // 词法分析器对象
+    vector<map<int, vector<Record>>> Infos; // 存储符号表信息
+    vector<string> lines; // 存储输入的代码行
+    set<string> Def{"int", "double", "char", "bool"}; // 定义的基本类型
+    vector<Record> synbl; // 存储符号表记录
+    vector<Record> arr; // 存储数组记录
 
+    // 构造函数
+    SymTable(const Lexer& lexer)
+        : lex(lexer) {}
+
+    // 获取符号表
     void Get_SymTable();
+    // 创建新的记录并添加到符号表中
     void Get_NewRecord(const string& type, int range, vector<Record>& S, int line, int name);
+    // 弹出指定范围的记录
     void Pop_Record(int range, map<int, vector<Record>>& InfoTable);
+    // 打印符号表
     void Print_Table(const map<int, vector<Record>>& InfoTable);
+    // 打印标准符号表
     void Print_RegularTable();
 };
 
