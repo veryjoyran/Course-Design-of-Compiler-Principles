51a24bf3b86b18803ca78a88354feb6d8addc9e8
joyran <2574202043@qq.com>
2024-06-10 21:04:36 +0800

    增加complier类，方便后续调用

--- include/complier.h
+++ include/complier.h
@@ -13,7 +13,32 @@ void lexical_test();
 void grammar_test();
 void lexer_test();
 void parser_test();
+void run();
 
+
+class Complier {
+public:
+    Grammar grammar;
+    string origin;
+    Lexer lexer;
+    vector<Token> tokens;
+    Parser parser;
+    vector<Quat> quats;
+    SymTable symtbl;
+    vector<string> lines;
+    Opt opt;
+    LexicalAnalyzer lexical;
+
+
+    int get_opt_type(string op);
+    string get_t(string str);
+    void Init_Gram();
+    void Lexical_Analysis();
+    void Grammatical_Analysis();
+    void Init_SymTable();
+    void Get_Quats();
+    void Init_opt();
+};
 
 
 
